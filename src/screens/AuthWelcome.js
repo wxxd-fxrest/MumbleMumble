2e5542f09615a981bf48b91945823ee39fe05ea3
fxrest704 <fxrest704@gmail.com>
2023-10-12 18:37:14 +0900

    loadig 화면 수정

--- src/screens/AuthWelcome.js
+++ src/screens/AuthWelcome.js
@@ -13,54 +13,12 @@ const AuthWelcome = () => {
     const circle2Scale = new Animated.Value(1);
 
     useEffect(() => {
-        Animated.sequence([
-            Animated.timing(circle1Scale, {
-                toValue: 1.2,
-                duration: 1000,
-                useNativeDriver: false,
-                easing: Easing.elastic(0), 
-            }),
-            Animated.timing(circle1Scale, {
-                toValue: 1,
-                duration: 1000,
-                useNativeDriver: false,
-                easing: Easing.elastic(0),
-            }),
-            Animated.timing(circle2Scale, {
-                toValue: 1.2,
-                duration: 1000,
-                useNativeDriver: false,
-                easing: Easing.elastic(0),
-            }),
-            Animated.timing(circle2Scale, {
-                toValue: 1,
-                duration: 1000,
-                useNativeDriver: false,
-                easing: Easing.elastic(2),
-            }),
-        ]).start();
         setTimeout(() => navigation.navigate("Login"), 2000);
     }, []);
 
     return (
         <Container isDark={isDark}>
-            <Animated.View
-                    style={{
-                    backgroundColor: isDark ? "rgba(9, 93, 158, 0.35)" : "rgba(221, 84, 15, 0.35)",
-                    borderRadius: 1000,
-                    padding: hp(5),
-                    transform: [{ scale: circle1Scale }],
-                }}>
-                <Animated.View
-                    style={{
-                        backgroundColor: isDark ? "rgba(9, 93, 158, 0.55)" : "rgba(221, 84, 15, 0.55)",
-                        borderRadius: 1000,
-                        padding: hp(0.5),
-                        transform: [{ scale: circle2Scale }],
-                    }}>
-                    <LogoImage source={isDark ? DarkLogo : LightLogo} />
-                </Animated.View>
-            </Animated.View>
+            <LogoImage source={isDark ? DarkLogo : LightLogo} />
         </Container>
     );
 };
